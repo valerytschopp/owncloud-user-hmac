9071b1adfb1511d30c6287fbb9dba3ecefa003b1
Valery Tschopp <valery.tschopp@gmail.com>
2014-01-15 14:07:07 +0100

    longer input field. Text added.

--- templates/settings.php
+++ templates/settings.php
@@ -1,8 +1,11 @@
-<form id="user_hmac" action="#" method="post">
-        <fieldset class="personalblock">
-                <h2><?php p($l->t('HMAC Authentication Backend'));?></h2>
-		<label for="user_hmac_key"><?php p($l->t('HMAC Shared Secret Key'));?><input type="text" name="user_hmac_key" id="user_hmac_key" value="<?php p($_['user_hmac_key']);?>" /></label>
-                <input type="hidden" name="requesttoken" value="<?php p($_['requesttoken']);?>" />
-                <input type="submit" value="<?php p($l->t('Save'));?>" />
-        </fieldset>
+<form id="user_hmac_form" action="#" method="post">
+	<fieldset class="personalblock">
+		<h2><?php p($l->t('HMAC User Backend'));?></h2>
+		<label for="user_hmac_key"><?php p($l->t('HMAC Shared Secret Key'));?></label>
+		<input type="text" name="user_hmac_key" id="user_hmac_key" style="width:auto" size="50" value="<?php p($_['user_hmac_key']);?>" />
+		<input type="hidden" name="requesttoken" value="<?php p($_['requesttoken']);?>" />
+		<input type="submit" value="<?php p($l->t('Save'));?>" />
+		<br>
+		<em>It is recommended to use a HMAC shared secret key with a minimum length of 20 characters.</em>
+	</fieldset>
 </form>
