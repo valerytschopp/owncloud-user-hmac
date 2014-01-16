86d74102eeedb12f491202127327502bcab4a17d
Valery Tschopp <valery.tschopp@gmail.com>
2014-01-16 17:34:48 +0100

    log message updated.

--- user_hmac.php
+++ user_hmac.php
@@ -28,7 +28,7 @@ class OC_USER_HMAC extends OC_User_Backend {
 		if ( empty($this->user_hmac_key) ) {
 			$this->user_hmac_key = OC_Util::generateRandomBytes(32);
 			OCP\Config::setAppValue('user_hmac', 'user_hmac_key', $this->user_hmac_key);
-			OCP\Util::writeLog('user_hmac', 'generated HMAC key: ' . $this->user_hmac_key, OCP\Util::INFO);
+			OCP\Util::writeLog('user_hmac', 'Generated HMAC key: ' . $this->user_hmac_key, OCP\Util::INFO);
 		}
 	}
 
