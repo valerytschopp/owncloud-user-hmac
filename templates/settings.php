e28c37df6e0cea9c88d3e0ec62fc307524785f14
Valery Tschopp <valery.tschopp@gmail.com>
2014-01-16 16:50:46 +0100

    width:auto css style for key text input field.

--- templates/settings.php
+++ templates/settings.php
@@ -2,7 +2,7 @@
 	<fieldset class="personalblock">
 		<h2><?php p($l->t('HMAC User Backend'));?></h2>
 		<label for="user_hmac_key"><?php p($l->t('HMAC Shared Secret Key'));?></label>
-		<input type="text" name="user_hmac_key" id="user_hmac_key" style="width:auto" size="50" value="<?php p($_['user_hmac_key']);?>" />
+		<input type="text" name="user_hmac_key" id="user_hmac_key" style="width:auto;" size="50" value="<?php p($_['user_hmac_key']);?>" />
 		<input type="hidden" name="requesttoken" value="<?php p($_['requesttoken']);?>" />
 		<input type="submit" value="<?php p($l->t('Save'));?>" />
 		<br>
